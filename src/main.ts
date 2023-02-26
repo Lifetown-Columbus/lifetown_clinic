332878e1fbfd80a52dee5edfdc2646639f1b1dd9
Stephen Turley <turley714@gmail.com>
2023-02-26 11:36:01 -0500

    added css

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,16 @@ import path from "path";
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 900,
+    resizable: true,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
   });
   ipcMain.handle("ping", () => "pong");
   win.loadFile("index.html");
+  win.setMenu(null);
   win.webContents.openDevTools();
 };
 
