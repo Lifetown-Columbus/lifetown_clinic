1904dc8f31dbc1c13ac8289bfc9d7c6a387f7ae0
Stephen Turley <stephen@turley.dev>
2023-03-17 13:23:44 -0400

    removed electron added .tool-versions

--- src/models/lesson.ts
+++
@@ -1,7 +0,0 @@
-import { Model, ModelAttributes, Sequelize } from "sequelize";
-
-export default class Lesson extends Model {
-  static config(sequelize: Sequelize): void {
-    this.init({}, { sequelize });
-  }
-}
--- src/preload.ts
+++
@@ -1,5 +0,0 @@
-import { contextBridge, ipcRenderer } from "electron";
-
-contextBridge.exposeInMainWorld("api", {
-  search: (text: String) => ipcRenderer.invoke("search", text),
-});
