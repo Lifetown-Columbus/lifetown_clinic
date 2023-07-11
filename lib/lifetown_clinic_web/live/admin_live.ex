83c134a1e9fc8b75ce667e3b001b534708bc3b4c
Stephen Turley <stephen@turley.dev>
2023-07-11 14:00:28 -0400

    added date filters to admin page

--- lib/lifetown_clinic_web/live/admin_live.ex
+++ lib/lifetown_clinic_web/live/admin_live.ex
@@ -2,6 +2,24 @@ defmodule LifetownClinicWeb.AdminLive do
   use LifetownClinicWeb, :live_view
 
   def mount(_, _, socket) do
+    socket =
+      socket
+      |> assign(:start_date, nil)
+      |> assign(:end_date, nil)
+
     {:ok, socket}
+  end
+
+  def handle_event(
+        "filter_updated",
+        %{"start-date" => start_date, "end-date" => end_date},
+        socket
+      ) do
+    socket =
+      socket
+      |> assign(:start_date, start_date)
+      |> assign(:end_date, end_date)
+
+    {:noreply, socket}
   end
 end
