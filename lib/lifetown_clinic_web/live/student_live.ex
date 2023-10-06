f017adc4200d2d6a48678cacf5992d941935644b
Stephen Turley <stephen@turley.dev>
2023-10-05 21:24:57 -0400

    added ability to delete a student

--- lib/lifetown_clinic_web/live/student_live.ex
+++ lib/lifetown_clinic_web/live/student_live.ex
@@ -13,7 +13,21 @@ defmodule LifetownClinicWeb.StudentLive do
     socket =
       socket
       |> assign(:student, student)
+      |> assign(:deleting, false)
 
     {:ok, socket}
+  end
+
+  def handle_event("delete", _, socket) do
+    {:noreply, assign(socket, :deleting, true)}
+  end
+
+  def handle_event("cancel_delete", _, socket) do
+    {:noreply, assign(socket, :deleting, false)}
+  end
+
+  def handle_event("confirm_delete", _, socket) do
+    Repo.delete!(socket.assigns.student)
+    {:noreply, push_navigate(socket, to: "/admin", replace: true)}
   end
 end
