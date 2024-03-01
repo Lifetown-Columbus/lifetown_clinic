0a58a4154208febe4690726eac4625016ed7c617
Stephen Turley <stephen@turley.dev>
2024-03-01 10:04:07 -0500

    All existing lessons will have a lesson number set to 1

--- priv/repo/migrations/20240229124124_add_number_to_lessons.exs
+++ priv/repo/migrations/20240229124124_add_number_to_lessons.exs
@@ -1,9 +1,12 @@
 defmodule LifetownClinic.Repo.Migrations.AddNumberToLessons do
   use Ecto.Migration
 
+  alias LifetownClinic.Repo
+  alias LifetownClinic.Schema.Lesson
+
   def change do
     alter table("lessons") do
-      add :number, :integer
+      add :number, :integer, default: 1
     end
   end
 end
